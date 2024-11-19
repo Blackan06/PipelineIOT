037c2c72f219d119ad7dbacff7b9861c79a5ffa1
AnhKiet <huynhanhkiet2222@gmail.com>
2024-11-19 14:42:15 +0700

    Update

--- init.sql
+++ init.sql
@@ -1,1 +1,11 @@
-CREATE DATABASE iot_data;
+CREATE DATABASE iot_data WITH OWNER postgres TEMPLATE template0
+WHERE NOT EXISTS (
+  SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('iot_data')
+);
+
+CREATE TABLE IF NOT EXISTS astro_version_check (
+  singleton BOOLEAN NOT NULL, 
+  last_checked TIMESTAMP WITH TIME ZONE, 
+  last_checked_by TEXT, 
+  CONSTRAINT astro_version_check_pkey PRIMARY KEY (singleton)
+);
